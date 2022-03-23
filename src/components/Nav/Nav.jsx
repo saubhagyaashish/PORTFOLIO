f70ba2a13e8357364b43b9451e5083b1f57e585b
SAUBHAGYA ASHISH <WTKX86@motorolasolutions.com>
2022-03-23 16:55:02 +0530

    Nav Bar Added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {CgWorkAlt} from 'react-icons/cg'
+import {RiServiceFill} from 'react-icons/ri'
+import {FiPhoneCall} from 'react-icons/fi'
+import './nav.css'
 
 const Nav = () => {
-  return <div>Nav
-    
-  </div>;
+  const [ActiveNav, setActiveNav] = useState("#")
+  
+  return (
+    <nav>
+      <a href='#'  onClick={()=>setActiveNav('#')} className={ActiveNav==="#"?"active":""} > <AiOutlineHome/></a>
+      <a href='#about' onClick={()=>setActiveNav('#about')} className={ActiveNav==="#about"?"active":""}><AiOutlineUser/></a>
+      <a href='#experience' onClick={()=>setActiveNav('#experience')} className={ActiveNav==="#experience"?"active":""}><CgWorkAlt/></a>
+      <a href='#services' onClick={()=>setActiveNav('#services')} className={ActiveNav==="#services"?"active":""}><RiServiceFill/></a>
+      <a href='#contact' onClick={()=>setActiveNav('#contact')} className={ActiveNav==="#contact"?"active":""}><FiPhoneCall/></a>
+
+    </nav>
+  );
 };
 
 export default Nav;
