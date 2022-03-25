0da4d5f2fe23866c2c582a0e6314d4da05196154
SAUBHAGYA ASHISH <WTKX86@motorolasolutions.com>
2022-03-25 14:26:39 +0530

    About Me page added!

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "./About.css"
+import ME from "../../assets/about_me.jpeg";
+import {FaAward} from "react-icons/fa"
+import {VscFolderLibrary} from "react-icons/vsc"
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me!</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+          </div>
+
+          <div className="about_content">
+            <div className="about_cards">
+              <article className="about_card">
+                <FaAward className="about_icon"/>
+                <h5>Experience</h5>
+                <small>1+ Years Working</small>
+              </article>
+
+              <article className="about_card">
+                <VscFolderLibrary className="about_icon"/>
+                <h5>Projects</h5>
+                <small>3 completed</small>
+              </article>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta excepturi eius necessitatibus delectus dolore quisquam doloribus, aliquam voluptate iure fugiat voluptas repudiandae facere esse fugit. Pariatur molestiae et impedit accusamus.
+            </p>
+            <a href="#contact" className="btn btn-primary">Let's Talk</a>
+          </div>
+        </div>
+      
+    </section>
+  );
 };
 
 export default About;
