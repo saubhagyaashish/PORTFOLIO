65e01e5c0c6abcca1c4f89fbe6d10d6d057fb59f
SAUBHAGYA ASHISH <WTKX86@motorolasolutions.com>
2022-04-04 15:52:50 +0530

    Testimonial Page Added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React from "react";
+import localRedux from "../../assets/localStorage_vs_ReduxPersist.jpeg";
+import reactFirestore from "../../assets/reactFirestore.jpeg";
+
+import "./portfolio.css";
+
+const ImgData = [
+  {
+    id: 1,
+    image: localRedux,
+    title: "Local Storage vs Redux Persist",
+  },
+  {
+    id: 2,
+    image: reactFirestore,
+    title: "React + Google Firebase: Complete Tutorial",
+  },
+];
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent</h5>
+      <h2>Works</h2>
+
+      <div className="container portfolio_container">
+        {ImgData.map((e) => { return(
+          <article className="portfolio_item">
+            <div className="portfolio_item-image">
+              <img src={e.image} />
+            </div>
+            <h3>{e.title}</h3>
+            <div className="portfolio_item-cta">
+              <a href="https://www.github.com" className="btn">
+                GitHub
+              </a>
+            </div>
+          </article>);
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React from "react";
+import "./testimonial.css";
+import AKD from "../../assets/AKD.JPG";
+import AD from "../../assets/AD.jpg";
+
+const data = [
+  {
+    id: 1,
+    name: "Angana Dawn",
+    designation:"R&D Engineer",
+    comapny: "NOKIA",
+    image: AD,
+    review:
+      "One of the most promising and creative person I have ever come across. Saubhagya is full of ideas. And he is best in what he does.",
+  },
+  {
+    id: 2,
+    name: "Abhay Kumar Dhar",
+    designation:"Engineer",
+    comapny: "L&T Technology Services",
+    image: AKD,
+    review:
+      "",
+  },
+];
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <section id="testimonials">
+      <h5>Check out</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonial_container">
+        {data.map((e) => {
+          return (
+            <article className="testimonial">
+              <div className="uppercard">
+                <div className="testimonials_avatar">
+                  <img src={e.image} alt="" />
+                </div>
+              </div>
+              <h5 className="testimonial_name">{e.name}</h5>
+              <small className="testimonials_review">{e.review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
